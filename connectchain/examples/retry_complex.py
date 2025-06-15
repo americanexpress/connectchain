7abcd00d243fb99feabe39cc76063e763c820038
Benjamin R. Laney <professionallaney@gmail.com>
2025-06-14 18:32:38 -0700

    Update new file headers.

--- connectchain/examples/retry_complex.py
+++ connectchain/examples/retry_complex.py
@@ -1,3 +1,14 @@
+# Copyright 2025 American Express Travel Related Services Company, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
 """This example demonstrates how to use the LCELRetry class to retry a chain of runnables in case of
 exceptions. The example simulates a database connection that may fail, and a model that may fail
 due to a firewall issue or a timeout.
